a9633634ca200d3b0dfde937d0d7c8f4038ce0b4
SamK-ie <g00438813@atu.ie>
2026-05-03 19:27:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G0438813',
+  webDir: 'www'
+};
+
+export default config;
